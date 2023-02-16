cfbcd90e37394658802d92227c9200cb44e3a37f
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-16 21:02:34 +0100

    Lifecycle Hooks

--- Beyond the fundamentals/app.js
+++ Beyond the fundamentals/app.js
@@ -3,8 +3,30 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
-  }
+  },
+  beforeCreate() {
+    console.log('beforCreate() function called: ',this.message)
+  },
+  created() {
+    console.log('created() function called: ',this.message)
+  },
+  beforeMount() {
+    console.log('beforeMount() function called: ',this.$el)
+  },
+  mounted() {
+    console.log('mounted() function called: ',this.$el)
+  },
+  beforeUpdate() {
+    console.log('beforUpdate() function called:')
+  },
+  updated() {
+    console.log('update() function called:')
+  },
+  beforeUnmount() {
+    console.log('beforUnmounted() function called:')
+  },
+  unmounted() {
+    console.log('unmounted() function called:')
+  },
 })
-setTimeout(()=>{
-    vm.mount("#app")
-},3000);
+vm.mount('#app')
