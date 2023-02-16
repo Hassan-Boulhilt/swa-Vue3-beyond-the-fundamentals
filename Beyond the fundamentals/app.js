fb3722d067ba912e2cb12a73dbbc54b3d93dd42a
hassan-boulhilt <hboulhilt@gmail.com>
2023-02-16 22:38:14 +0100

    Components

--- Beyond the fundamentals/app.js
+++ Beyond the fundamentals/app.js
@@ -1,32 +1,13 @@
 let vm = Vue.createApp({
+  
+})
+vm.component('hello', {
+  template: `<h1>{{message}}</h1>`,
   data() {
     return {
-      message: "Hello world!"
+      message: "Hello World!",
     }
-  },
-  beforeCreate() {
-    console.log('beforCreate() function called: ',this.message)
-  },
-  created() {
-    console.log('created() function called: ',this.message)
-  },
-  beforeMount() {
-    console.log('beforeMount() function called: ',this.$el)
-  },
-  mounted() {
-    console.log('mounted() function called: ',this.$el)
-  },
-  beforeUpdate() {
-    console.log('beforUpdate() function called:')
-  },
-  updated() {
-    console.log('update() function called:')
-  },
-  beforeUnmount() {
-    console.log('beforUnmounted() function called:')
-  },
-  unmounted() {
-    console.log('unmounted() function called:')
-  },
+
+  } 
 })
 vm.mount('#app')
